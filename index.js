1cdebd4176286cb8007a78a65d4d21dc58fb7dba
phuthanh99 <huynhnguyenphuthanh@gmail.com>
2022-04-12 10:02:57 +0700

    change

--- index.js
+++ index.js
@@ -1,44 +1,68 @@
-﻿const express = require('express');
+﻿const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 
-const userRouter = require('./routes/users')
-const authRouter = require('./routes/auth')
-const postRouter = require('./routes/post')
-
-
+const userRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
 
 dotenv.config();
 const port = process.env.PORT;
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, (err) => {
-    if (!err) {
-        console.log('MongoDB Connection Succeeded.');
-    } else {
-        console.log('Error in DB connection');
-    }
-});
-app.use(cors());
+mongoose.connect(
+	process.env.MONGO_URL,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	(err) => {
+		if (!err) {
+			console.log("MongoDB Connection Succeeded.");
+		} else {
+			console.log("Error in DB connection");
+		}
+	}
+);
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.use(cors());
 
 // middleware
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("common"))
+app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "public/images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, req.body.name);
+	},
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+	try {
+		return res.status(200).json("File uploded successfully");
+	} catch (error) {
+		console.error(error);
+	}
+});
 
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 
-
 app.listen(port, () => {
-    console.log(`Backend sever is running, listening on port http://localhost:${port}`);
+	console.log(
+		`Backend sever is running, listening on port http://localhost:${port}`
+	);
 });
