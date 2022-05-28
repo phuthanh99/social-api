1411c5c10826856e41064d12d025d2c2398c8bd9
phuthanh99 <huynhnguyenphuthanh@gmail.com>
2022-05-28 18:42:44 +0700

    change code

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.use("/api/search", seacrchRoute);
 
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "https://pusta.vercel.app/",
 		methods: ["GET", "POST"],
 	},
 });
